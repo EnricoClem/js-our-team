4843e71ee49188e6b1ae20ac62ac824ff0e0e6c4
EnricoClem <enrico.clem94@gmail.com>
2024-03-18 15:25:51 +0100

    ADD - all 6 objects

--- app.js
+++ app.js
@@ -8,10 +8,40 @@
 // Trasformare la stringa foto in una immagine effettiva
 // Organizzare i singoli membri in card/schede
 
-const wayne = {
+const wayneB = {
     "name": "Wayne Barnett",
     "job": "Founder & CEO",
     "photo": "wayne-barnett-founder-ceo.jpg"
+}
+
+const angelaC = {
+    "name": "Angela Caroll",
+    "job": "Chief Editor",
+    "photo": "angela-caroll-chief-editor.jpg"
+}
+
+const walterG = {
+    "name": "Walter Gordon",
+    "job": "Office Manager",
+    "photo": "walter-gordon-office-manager.jpg"
+}
+
+const angelaL = {
+    "name": "Angela Lopez",
+    "job": "Social Media Manager",
+    "photo": "angela-lopez-social-media-manager.jpg"
+}
+
+const scottE = {
+    "name": "Scott Estrada",
+    "job": "Developer",
+    "photo": "scott-estrada-developer.jpg"
+}
+
+const barbaraR = {
+    "name": "Barbara Ramos",
+    "job": "Graphic Designer",
+    "photo": "barbara-ramos-graphic-designer.jpg"
 }
 
 console.log(wayne['name'], wayne['job'], wayne['photo'])
