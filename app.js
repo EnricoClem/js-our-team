e78d9edf86aa39611768bb1fcf86c0d1be9c2230
EnricoClem <enrico.clem94@gmail.com>
2024-03-18 15:13:39 +0100

    SETUP - js instruction

---
+++ app.js
@@ -0,0 +1,10 @@
+// Creare i singoli oggetti con le tre key richieste:
+    // Nome
+    // Ruolo
+    // Foto
+// Creare un array contenente tutti gli oggetti
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+// Stampare le stesse informazioni su DOM semplicemente come stringhe
+// Trasformare la stringa foto in una immagine effettiva
+// Organizzare i singoli membri in card/schede
+
